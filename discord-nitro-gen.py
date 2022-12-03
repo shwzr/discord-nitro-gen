f13ed943a97c1a6984d1ffa7557640ea4962fe2f
shwzr <113097122+shwzr@users.noreply.github.com>
2022-12-03 18:11:30 +0100

    Add files via upload

---
+++ discord-nitro-gen.py
@@ -0,0 +1,9 @@
+import random
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+for i in range(100000):
+    first = ''.join((random.choice(chars) for i in range(16)))
+    result = "https://discord.gift/" + first
+    output = open("gifts.txt", "a")
+    output.write(result + "\n")
+
