1638e58704f4881e9611710a62154d62142f8fee
shwzr <113097122+shwzr@users.noreply.github.com>
2022-12-03 18:11:15 +0100

    Delete discord-nitro-gen.py

--- discord-nitro-gen.py
+++
@@ -1,9 +0,0 @@
-import random
-chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
-for i in range(100000):
-    first = ''.join((random.choice(chars) for i in range(16)))
-    result = "https://discord.gift/" + first
-    output = open("tokens.txt", "a")
-    output.write(result + "\n")
-
